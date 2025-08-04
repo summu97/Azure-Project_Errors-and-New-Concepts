1daae9bb01224db67c00555edd73de9feff3f8d7
summu97 <135579543+summu97@users.noreply.github.com>
2025-08-04 11:48:25 +0530

    Update notifyTeam.groovy

--- Jenkins/jenkins email notification using libraries/notifyTeam.groovy
+++ Jenkins/jenkins email notification using libraries/notifyTeam.groovy
@@ -4,24 +4,24 @@ def call(Map config = [:]) {
     def buildNumber = config.buildNumber ?: 'N/A'
     def buildUrl = config.buildUrl ?: '#'
     def type = config.type ?: 'info'
+    def approver    = config.approvedBy ?: 'N/A'
+    def buildUser   = config.triggeredBy ?: 'N/A'
 
-    def recipients = [
-        'vbboya@afmsagaftrafund.org',
-        'vsboyina@afmsagaftrafund.org',
-        'dkgiddaluri@afmsagaftrafund.org',
-        'nganesh@afmsagaftrafund.org',
-        'vkjuvvadi@afmsagaftrafund.org',
-        'vkrmuppidi@afmsagaftrafund.org',
-        'psagar@afmsagaftrafund.org',
-        'sradhamolla@afmsagaftrafund.org',
-        'bpnunna@afmsagaftrafund.org',
-        'sbade@afmsagaftrafund.org',
-        'dkundo@afmsagaftrafund.org',
-        'rkrishnavalluri@afmsagaftrafund.org'
-    ].join(',')
+    // Git commit info
+    def commitMessage = sh(script: "git log -1 --pretty=%s", returnStdout: true).trim()
+    def commitHash    = sh(script: "git log -1 --pretty=%h", returnStdout: true).trim()
+    def commitAuthor  = sh(script: "git log -1 --pretty=%an", returnStdout: true).trim()                        
+
+
+    // Email Recipients
+   // def recipients = [
+  //      'vbboya@afmsagaftrafund.org'
+  //  ].join(',')
 
     def subject = ''
     def body = ''
+    def teamsMessage = ''
+    def color = '#808080'  // default gray
 
     switch (type) {
         case 'approval':
@@ -30,47 +30,78 @@ def call(Map config = [:]) {
                 <h3>🚦 Jenkins Build Approval Required</h3>
                 <p><strong>Service:</strong> ${service}</p>
                 <p><strong>Environment:</strong> ${env}</p>
+
+
                 <p><strong>Build Number:</strong> ${buildNumber}</p>
                 <p><a href="${buildUrl}">Click here to review and approve</a></p>
                 <hr/>
                 <p>Please approve the deployment to proceed.</p>
             """
+            teamsMessage = "**🔔 Deployment Approval Needed**\n\n📦 Service : **${service}**\n\n🌍 Environment : **${env}**\n\n👤 Triggered By : **${buildUser}**\n\n📝 Commit Message : **${commitMessage}**\n\n🧑‍💻 Commit Author : **${commitAuthor}**\n\n"
+            color = '#FFA500'  // orange
             break
 
         case 'success':
-            subject = "✅ Success: ${service} deployed to ${env}"
+            subject = "**🥳: ${service} deployed to ${env}"
             body = """
                 <h3>✅ Deployment Successful</h3>
                 <p><strong>Service:</strong> ${service}</p>
                 <p><strong>Environment:</strong> ${env}</p>
+
+
+
                 <p><strong>Build Number:</strong> ${buildNumber}</p>
                 <p><a href="${buildUrl}">View Pipeline</a></p>
             """
+            teamsMessage = "**🥳 Deployment Success**\n\n📦 Service : **${service}**\n\n🌍 Environment : **${env}**\n\n👤 Triggered By : **${buildUser}**\n\n📝 Commit Message : **${commitMessage}**\n\n🧑‍💻 Commit Author : **${commitAuthor}**\n\n"
+            color = '#28a745'  // green
             break
 
         case 'failure':
-            subject = "❌ Failed: ${service} build/deploy in ${env}"
+            subject = "😭 Failed: ${service} build/deploy in ${env}"
             body = """
                 <h3>❌ Build or Deployment Failed</h3>
                 <p><strong>Service:</strong> ${service}</p>
                 <p><strong>Environment:</strong> ${env}</p>
+
+
+
                 <p><strong>Build Number:</strong> ${buildNumber}</p>
                 <p><a href="${buildUrl}">View Pipeline</a></p>
                 <hr/>
                 <p>Please check the logs and take necessary action.</p>
             """
+            teamsMessage = "**😭 Deployment Failed**\n\n📦 Service : **${service}**\n\n🌍 Environment : **${env}**\n\n👤 Triggered By : **${buildUser}**\n\n📝 Commit Message : **${commitMessage}**\n\n🧑‍💻 Commit Author : **${commitAuthor}**\n\n"
+            color = '#dc3545'  // red
             break
 
         default:
             subject = "ℹ️ Notification: ${service} in ${env}"
             body = "<p>No additional details.</p>"
+            teamsMessage = "ℹ️ **Notification** for ${service} in ${env}"
     }
 
-    echo "📧 Sending ${type} notification email to ${recipients}"
-    mail(
-        to: recipients,
-        subject: subject,
-        body: body,
-        mimeType: 'text/html'
+    // Send Email
+   // echo "📧 Sending ${type}  email to ${recipients}"
+    // mail(
+    //     to: recipients,
+    //     subject: subject,
+    //     body: body,
+    //     mimeType: 'text/html'
+    // )
+
+
+
+
+    // Send Teams Notification
+    def teamsWebhook = 'https://prod-13.centralindia.logic.azure.com:443/workflows/db59c2860a05449081480ba631850e88/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=CDckBf0gKwFAC8ddpcW-rMurNuHgBnRvojiIz69CHe8'
+
+    office365ConnectorSend(
+        webhookUrl: teamsWebhook,
+        message: teamsMessage,
+        status: type.capitalize(),
+        color: color,
+        adaptiveCards: true
     )
 }
+ 
